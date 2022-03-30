9b9099e09808a4e49eac369cc5f0275a2f02a80f
Frederik SCHLUTER <fschlute@e2r12p9.clusters.42paris.fr>
2022-03-30 16:55:09 +0200

    fixed unset variables for cd and pwd

--- src/built_in/cd.c
+++ src/built_in/cd.c
@@ -1,19 +1,39 @@
 #include "../minishell.h"
 
+char	*get_home_path(t_list *const *env);
+
 int	msh_cd(t_list **env, t_list **cmd, int index)
 {
 	t_cmd	*current_cmd;
 	char	s[2048];
+	char	*dest_path;
 
 	current_cmd = get_content(ft_lstget_element_by_index(*cmd, index));
 	if (current_cmd->args[1] == NULL)
+		dest_path = get_home_path(env);
+	else
+		dest_path = current_cmd->args[1];
+	if (dest_path == NULL)
+		return (1);
+	if (chdir(dest_path) == -1)
 	{
-		ft_putendl_fd("cd: No such file or directory", 2);
-		return (2);
-	}
-	if (chdir(current_cmd->args[1]) == -1)
+		perror(dest_path);
 		return (errno);
+	}
 	if (update_env(env, "PWD", getcwd(s, 2048), ENV_REPLACE_VAR))
 		return (1);
 	return (0);
+}
+
+char	*get_home_path(t_list *const *env)
+{
+	t_dict_entry	*home;
+
+	home = get_entry_by_key(*env, "HOME");
+	if (home == NULL)
+	{
+		ft_putendl_fd("cd: HOME not set", 2);
+		return (NULL);
+	}
+	return (home->value);
 }
--- src/built_in/pwd.c
+++ src/built_in/pwd.c
@@ -2,13 +2,18 @@
 
 int	msh_pwd(t_list **env, t_list **cmd, int index)
 {
-	t_dict_entry	*pwd;
+	char	*pwd;
+	char	s[2048];
 
-	pwd = get_entry_by_key(*env, "PWD");
+	pwd = getcwd(s, 2048);
 	if (pwd == NULL)
-		return (1);
-	ft_putendl_fd(pwd->value, 1);
+	{
+		perror("pwd");
+		return (errno);
+	}
+	ft_putendl_fd(pwd, 1);
 	return (0);
 	(void) cmd;
+	(void) env;
 	(void) index;
 }
